6d8348e30660af9a926a38ed6cbfc4e754cba099
MikeMNelhams <mikemnelhams@gmail.com>
2024-07-15 20:53:55 +0100

    Changelist: - Ace card effect unit tests

---
+++ Assets/Tests/EditMode/CardComboAceTests.cs
@@ -0,0 +1,142 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Karma.BasicBoard;
+using Karma.Board;
+using Karma.Cards;
+
+public class CardComboAceTests
+{
+    [Test]
+    public void AceFlipsHands()
+    {
+        List<List<List<int>>> playerCardValues = new()
+        {
+            new() { new() { }, new() { }, new() { } }
+        };
+
+        List<int> drawCardValues = new() { };
+        List<int> playCardValues = new() { };
+        List<int> burnCardValues = new() { };
+
+        BasicBoard board = BoardFactory.MatrixStart(playerCardValues, drawCardValues, playCardValues, burnCardValues, boardPlayOrder: BoardPlayOrder.DOWN, boardTurnOrder: BoardTurnOrder.LEFT);
+
+        board.StartTurn();
+
+        PlayerController testController = new();
+
+        CardsList cards = new(new List<int>() { 14 }, CardSuit.Hearts);
+
+        board.PlayCards(cards, testController);
+
+        Assert.AreEqual(BoardPlayOrder.DOWN, board.PlayOrder);
+        Assert.AreEqual(BoardTurnOrder.LEFT, board.TurnOrder);
+        Assert.AreEqual(1, board.EffectMultiplier);
+        Assert.True(board.HandsAreFlipped);
+    }
+
+    [Test]
+    public void AceFlipsHandsBackToNotFlipped()
+    {
+        List<List<List<int>>> playerCardValues = new()
+        {
+            new() { new() { }, new() { }, new() { } }
+        };
+
+        List<int> drawCardValues = new() { };
+        List<int> playCardValues = new() { };
+        List<int> burnCardValues = new() { };
+
+        BasicBoard board = BoardFactory.MatrixStart(playerCardValues, drawCardValues, playCardValues, burnCardValues, boardPlayOrder: BoardPlayOrder.DOWN, boardTurnOrder: BoardTurnOrder.LEFT, handsAreFlipped: true);
+
+        board.StartTurn();
+
+        PlayerController testController = new();
+
+        CardsList cards = new(new List<int>() { 14 }, CardSuit.Hearts);
+
+        board.PlayCards(cards, testController);
+
+        Assert.AreEqual(BoardPlayOrder.DOWN, board.PlayOrder);
+        Assert.AreEqual(BoardTurnOrder.LEFT, board.TurnOrder);
+        Assert.AreEqual(1, board.EffectMultiplier);
+        Assert.False(board.HandsAreFlipped);
+    }
+
+    [Test]
+    public void AceGroupComboEven()
+    {
+        List<List<List<int>>> playerCardValues = new()
+        {
+            new() { new() { }, new() { }, new() { } }
+        };
+
+        List<int> drawCardValues = new() { };
+        List<int> playCardValues = new() { };
+        List<int> burnCardValues = new() { };
+
+        BasicBoard board = BoardFactory.MatrixStart(playerCardValues, drawCardValues, playCardValues, burnCardValues);
+
+        board.StartTurn();
+
+        PlayerController testController = new();
+
+        CardsList cards = new(new List<int>() { 14, 14 }, CardSuit.Hearts);
+
+        board.PlayCards(cards, testController);
+
+        Assert.AreEqual(1, board.EffectMultiplier);
+        Assert.False(board.HandsAreFlipped);
+    }
+
+    [Test]
+    public void AceGroupComboOdd()
+    {
+        List<List<List<int>>> playerCardValues = new()
+        {
+            new() { new() { }, new() { }, new() { } }
+        };
+
+        List<int> drawCardValues = new() { };
+        List<int> playCardValues = new() { };
+        List<int> burnCardValues = new() { };
+
+        BasicBoard board = BoardFactory.MatrixStart(playerCardValues, drawCardValues, playCardValues, burnCardValues);
+
+        board.StartTurn();
+
+        PlayerController testController = new();
+
+        CardsList cards = new(new List<int>() { 14, 14, 14 }, CardSuit.Hearts);
+
+        board.PlayCards(cards, testController);
+
+        Assert.AreEqual(1, board.EffectMultiplier);
+        Assert.True(board.HandsAreFlipped);
+    }
+
+    [Test]
+    public void AceGroupComboOddSixFilled()
+    {
+        List<List<List<int>>> playerCardValues = new()
+        {
+            new() { new() { }, new() { }, new() { } }
+        };
+
+        List<int> drawCardValues = new() { };
+        List<int> playCardValues = new() { };
+        List<int> burnCardValues = new() { };
+
+        BasicBoard board = BoardFactory.MatrixStart(playerCardValues, drawCardValues, playCardValues, burnCardValues);
+
+        board.StartTurn();
+
+        PlayerController testController = new();
+
+        CardsList cards = new(new List<int>() { 14, 14, 14, 6, 14 }, CardSuit.Hearts);
+
+        board.PlayCards(cards, testController);
+
+        Assert.AreEqual(1, board.EffectMultiplier);
+        Assert.True(board.HandsAreFlipped);
+    }
+}
